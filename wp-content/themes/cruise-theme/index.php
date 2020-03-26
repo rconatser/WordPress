26a51d52865edc3d3fc1126852e02c9f54d65123
Rachel Conatser <10806252@my.uvu.edu>
2020-03-25 20:15:39 -0600

    completed unit 7 - added news/styles

--- wp-content/themes/cruise-theme/index.php
+++ wp-content/themes/cruise-theme/index.php
@@ -1,3 +1,10 @@
-<?php
-// Required for a Blog...
-?>
+<?php get_header(); ?>
+
+<div class="container news-post">
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php the_title( '<h1>','</h1>' ); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+</div>
+
+<?php get_footer(); ?>
